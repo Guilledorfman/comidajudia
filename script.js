c2febaad2d6001b97027a40042c91a7759b06688
Guillermo <guillermodorfman@gmail.com>
2021-10-20 17:08:06 -0300

    clase de ajax

---
+++ script.js
@@ -0,0 +1,445 @@
+//array ids de recetas guardadas en favoritos
+
+
+let stringToArray = [];
+let recetario;
+
+favoritesStorageArray()
+
+//convierte el string guardado en un array de numeros y se lo asigna a recetario.
+
+function favoritesStorageArray(){
+    let getFavString  =  localStorage.getItem('favoritesStorage');
+    if(getFavString){
+        stringToArray = getFavString.split(',');
+    
+        for(let i =0;i<stringToArray.length;i++){
+            stringToArray[i]= (parseInt(stringToArray[i]));
+        }
+        recetario = stringToArray;
+    }else{
+        recetario = [];
+    }
+}
+
+//le pasa el valor actualizado de recetario transformado en string al Localstorage
+
+function updateStorage(){
+    localStorage.setItem('favoritesStorage', recetario.toString());
+    favoritesStorageArray();
+}
+
+//Constructor de recetas
+class Recipe {
+    constructor (name, id, ingredientes, instrucciones, imagen, descripcion) {
+        this.name = name;
+        this.id = id;
+        this.ingredientes = ingredientes;
+        this.instrucciones = instrucciones;
+        this.imagen = imagen;
+        this.missingIngredients = [];    
+        this.descripcion = descripcion;
+        this.favorited = false;
+    }
+    
+    addFavorite(){
+        recetario.push(this.id);
+
+    }
+
+}
+
+
+//Array de recetas
+const recipes = [];
+
+$.get('./json/recipes.json', (response) =>{
+    response.forEach((recipe)=>{
+        recipes.push(recipe);
+    })
+    resetList();
+
+ })
+
+
+// borrar resultados y volver a generar la lista de recetas completa a partir del array recipes.
+
+const recipesList = document.getElementById('result');
+
+function resetList(){
+    recipesList.innerHTML="";
+
+    //Loading con JQuery
+    $('#result').prepend('<div id="loading"><img src="./img/loading.gif" alt=""></div>');
+    $('#result').css({
+        
+        overflow: 'hidden'
+    })
+    
+    recipes.forEach(e=>{
+        
+        let listItem = document.createElement('li');
+        $('#result>li').removeClass('visible')
+        $('#result>li').addClass('hidden')
+        listItem.addEventListener('click', ()=>{
+            hideRecetarioMenu();
+            createModal(e);
+        })
+        listItem.innerHTML = `  <img src="${e.imagen}" alt="${e.name}">
+        <div class="recipe-info">
+        <h4>${e.name}</h4>
+        <h5>${e.descripcion}</h5>
+        </div>`;
+        recipesList.appendChild(listItem);
+        
+    })
+
+    setTimeout(()=>{
+        $('#result>li').addClass('visible')
+        $('#result>li').removeClass('hidden')
+        $('#result').css({
+            overflow: 'auto'
+        })
+        $('result>li').removeClass('')
+        $('#loading').css({
+            opacity:0,
+            visibility : 'hidden'
+        })
+    },400)
+}
+
+
+
+let currentId = 0;
+
+function createModal(e){
+    currentId = e.id;
+
+    let modal = document.getElementById('modal');
+    modal.classList.toggle('visible')
+    modal.addEventListener('click', (e)=>{
+        if(e.target == modal){
+            closeModal(e);
+
+        }
+    })
+
+    let recipeModal = document.createElement('div');
+    recipeModal.classList.add('modal-cont')
+    recipeModal.innerHTML = `   <div class="descripcion">
+                                    <img src="${e.imagen}" alt="">
+                                    <div class="texto">
+                                    <h2>${e.name}</h2>
+                                    <p>${e.descripcion}</p>
+                                    </div>
+                                </div>
+                                <div class="receta-cont">
+                                    <p class="ingredientes">${e.ingredientes}</p>
+                                    <p class="receta">${e.instrucciones}</p>
+                                </div>`          
+
+      //creo agregar al recetario                       
+    let = favoriteheart = document.createElement('span');
+    favoriteheart.addEventListener('click',(e)=>{
+        favorite(e);
+        generarRecetario();
+    })
+    favoriteheart.classList.add('favoriteheart', 'material-icons-outlined')
+    favoriteheart.innerHTML = "favorite";
+
+    //si el id de la receta está en el array recetario, pinta el corazon de rojo
+    let found = recetario.find(element => element === currentId);
+    if(found){
+        favoriteheart.classList.add('favorite')
+    }
+
+    modal.appendChild(recipeModal);
+    recipeModal.appendChild(favoriteheart);
+
+}
+
+//funcion de agregar id de la receta al array favoritos, si el id ya se encuentra en el array, lo saca
+function favorite(e){
+    let found = recetario.find(element => element === currentId);
+
+    if(!found){
+        e.target.classList.toggle('favorite')
+        recetario.push(currentId);
+        updateStorage();
+    }else{
+        e.target.classList.toggle('favorite')
+        const recipeIndex = recetario.indexOf(found);
+        recetario.splice(recipeIndex,1);
+        updateStorage();
+    }
+
+}
+
+
+function closeModal(e){
+    document.getElementById('modal').innerHTML = ""
+    modal.classList.remove('visible')
+}
+
+
+// No se encuentran recetas con esos valores de busqueda.
+
+function showNoResults(){
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `  <img src="./img/arrow_left_circle_icon_136939.png" alt="BACK">
+    <div class="recipe-info">
+    <h4>No se encontraron recetas</h4>
+    <h5>Presione este cuadro o elimine los tags para resetear la búsqueda</h5>
+    </div>`;
+    recipesList.appendChild(listItem);
+    listItem.addEventListener('click', ()=>{
+        searchWords.innerHTML= ""
+        resetList();
+    })
+}
+
+// crea los tags de búsqueda, el parametro (e) es el array ingredientesDisponibles si buscas por ingredientes
+// o el valor de userInput si buscas por nombre de receta.
+
+function showTags(e){
+    if(e.length>0){
+
+        let searchWords = document.getElementById('searchWords');
+        let deleteTags = document.createElement('b');
+        deleteTags.addEventListener('click', ()=>{
+            searchWords.innerHTML= ""
+            ingredientesDisponibles = ""
+            resetList();
+        })
+        deleteTags.innerHTML = 'X';
+        searchWords.appendChild(deleteTags);
+        if(e==ingredientesDisponibles){
+
+            e.forEach(e=>{
+                let tagItem = document.createElement('li');
+                tagItem.innerHTML = `${e}`;
+                searchWords.appendChild(tagItem);
+            })
+        }else{
+            let tagItem = document.createElement('li');
+            tagItem.innerHTML = `${e}`;
+            searchWords.appendChild(tagItem);
+        }
+    }
+}
+
+const searchIngredients = document.getElementById('searchIngredients');
+const searchRecipes = document.getElementById('searchRecipes');
+const recipeInput = document.getElementById('recipeInput');
+const ingredientInput = document.getElementById('ingredientInput');
+
+// interaccion con las solapas de busqueda
+// se resetean todos los valores al cambiar de una solapa a la otra para que no continue el valor de un input en la siguiente busqueda
+
+let solapaUno = true;
+
+document.getElementById('searchIngredientsCont').classList.toggle('onTop');
+
+searchIngredients.addEventListener('click', ()=>{
+    if (solapaUno == false){
+        recipeInput.value = ""
+        ingredientInput.value = ""
+        searchWords.innerHTML= ""
+        resetList()
+        document.getElementById('searchNameCont').classList.toggle('onTop');
+        document.getElementById('searchIngredientsCont').classList.toggle('onTop');
+        solapaUno = true;
+    }
+})
+
+searchRecipes.addEventListener('click', ()=>{
+    if (solapaUno == true){
+        recipeInput.value = ""
+        ingredientInput.value = ""
+        searchWords.innerHTML= ""
+        resetList()
+        document.getElementById('searchNameCont').classList.toggle('onTop');
+        document.getElementById('searchIngredientsCont').classList.toggle('onTop');
+        solapaUno = false;
+    }
+})
+
+
+// Busqueda por ingredientes. Al ingresar separado por ',' se eliminan los espacios y 
+//se agrega un nuevo elemento al array ingredientesDisponibles
+let userInput;
+let ingredientesDisponibles = [];
+
+function enterSearchIng(){
+    userInput = ingredientInput.value;
+    if(userInput){
+        ingredientesDisponibles = userInput.split(',').filter(tag => tag.trim()
+        !=='').map(tag=> tag.trim());
+    }  
+    ingredientInput.value = ""
+    searchByIngredients(ingredientesDisponibles);
+    searchWords.innerHTML= ""
+    showTags(ingredientesDisponibles);
+}
+
+//activa con enter o apretando el boton
+
+ingredientInput.addEventListener('keypress', (e)=>{
+    if (e.key === 'Enter'){
+        enterSearchIng();
+        
+    }
+})
+
+document.getElementById('searchIngBtn').addEventListener('click', ()=>{
+    enterSearchIng();
+})
+
+
+//busqueda por nombre
+
+function enterSearchName(){
+    userInput = recipeInput.value;
+    if(userInput){
+        recipeInput.value = ""
+        searchWords.innerHTML= ""
+        showTags(userInput);
+        searchByName(userInput);
+    }  
+}
+
+//activa con enter o apretando el boton
+
+recipeInput.addEventListener('keypress', (e)=>{
+    if (e.key === 'Enter'){
+        
+        enterSearchName();
+    }
+})
+
+document.getElementById('searchNameBtn').addEventListener('click', ()=>{
+    enterSearchName();
+})
+
+
+// codigo de comparacion de arrays en busqueda por ingredientes
+
+function searchByIngredients(e){
+    recipesList.innerHTML = "";
+    
+    let recipeFound;
+    let recetasPosibles = [];
+    
+    for (let i = 0; i < recipes.length; i++){
+        recipeFound = true;
+        for (let j = 0; j< ingredientesDisponibles.length; j++) {
+            if (recipes[i].ingredientes.indexOf(ingredientesDisponibles[j]) === -1) {
+                recipeFound = false;
+                break;
+            }
+        }
+        if (recipeFound){
+            recetasPosibles.push(recipes[i]);
+        }
+    }
+    if(recetasPosibles.length >= 1){
+        showRecipes(recetasPosibles);
+    }else{
+        showNoResults();
+    }
+}
+
+//codigo de comparacion en busqueda por nombre
+
+function searchByName(e){
+    recipesList.innerHTML = "";
+    const inputName = e.toUpperCase();
+    
+    let recipeFound;
+    let recetasPosibles = [];
+    
+    for(let i=0; i <recipes.length; i++){
+        recipeFound = recipes[i].name.toUpperCase().includes(inputName)
+        if(recipeFound){
+            recetasPosibles.push(recipes[i]);
+        }
+    }
+    if(recetasPosibles.length>0){
+        showRecipes(recetasPosibles);
+    }else{
+        showNoResults();
+        
+    }
+    
+}
+
+//Muestra los resultados del array recetasPosibles.
+
+function showRecipes(e){
+    e.forEach(e=>{
+        
+        let listItem = document.createElement('li');
+        listItem.addEventListener('click', ()=>{
+            hideRecetarioMenu();
+            createModal(e)
+        })
+        listItem.innerHTML = `  <img src="${e.imagen}" alt="${e.name}">
+        <div class="recipe-info">
+        <h4>${e.name}</h4>
+        <h5>${e.descripcion}</h5>
+        </div>`;
+        recipesList.appendChild(listItem);
+    })
+}
+
+
+//solapa recetario
+
+const recetarioMenu = document.getElementById('recetario');
+const solapaRecetario = document.getElementById('solapaRecetario');
+
+solapaRecetario.addEventListener('click',()=>{
+    showRecetarioMenu();
+})
+
+function showRecetarioMenu(){
+    generarRecetario();
+    recetarioMenu.classList.toggle('active');
+}
+
+function hideRecetarioMenu(){
+    recetarioMenu.classList.remove('active');
+}
+
+function generarRecetario(){
+    const recetasGuardadas = document.getElementById('recetasGuardadas')
+    recetasGuardadas.innerHTML = '';
+    if (recetario.length==0){
+        recetasGuardadas.innerHTML = '<p><br><b>No hay nada por aquí</b><br>En esta sección podrás ver las recetas que guardaste en favoritos.</p>'
+    }else{
+        const tituloRecetario = document.createElement('div');
+        tituloRecetario.innerHTML = '<h2>Recetario</h2>Recetas guardadas'
+        recetasGuardadas.appendChild(tituloRecetario);
+        recetario.forEach(e=>{
+            const compararId = recipes.find(obj =>{
+                return obj.id === e
+            });
+    
+            let recetaGuardada = document.createElement('li');
+    
+            recetaGuardada.innerHTML = `<img class="recetarioImg" src="${compararId.imagen}" alt="${compararId.name}">
+                                        <div class="recipe-info">
+                                            <h4>${compararId.name}</h4>
+                                        </div>`;
+            recetaGuardada.addEventListener('click',()=>{
+                createModal(compararId);
+            })
+            recetasGuardadas.appendChild(recetaGuardada);
+        })
+
+    }
+    
+}
+
+
+
